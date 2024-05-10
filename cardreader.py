c485406441505b18826fea814864f70f01eae1b1
TheHeisenBurglar <vidgames16@gmail.com>
2024-05-10 19:19:02 -0400

    First commit

---
+++ cardreader.py
@@ -0,0 +1,49 @@
+#Designed and developed by: Ibrahim Shehadeh
+#Created on: 10-may-2024
+#Desc: 
+ #Prompts the user to select the anki exported .txt file and converts the card titles to an array.
+#Github repo:
+    #
+
+from tkinter import *
+import re
+from tkinter import filedialog
+
+text = "empty"
+ready = False
+towrite = []
+def browseFiles():
+    filename = filedialog.askopenfilename(initialdir = "/",
+                                          title = "Select a File",
+                                          filetypes = (("Text files",
+                                                        "*.txt*"),
+                                                       ("all files",
+                                                        "*.*")))
+      
+    string = (filename)
+    return string
+def extractJapaneseText(textline):
+    txt = ""
+    japanese_pattern = re.compile(r'[\u3040-\u309F\u30A0-\u30FF\u4E00-\u9FFF\u3000-\u303F]')
+    if "font-size: 50px;" in textline:
+        for char in textline:
+            if(japanese_pattern.search(char)):
+                txt += char
+        if(txt != ""):
+            return txt
+        else:
+            return 
+def toFile(toappend, array):
+    array.append(word)
+
+text = browseFiles()      
+print(text) 
+file = open(text, "r")   
+if text:
+    with open(text, "r", encoding="utf-8") as file:
+        for line in file:
+            word = extractJapaneseText(line)
+            if word:
+                toFile(word, towrite)
+print("array: ", towrite)
+file.close() 
